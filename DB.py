090bcfc7c438e898897029c04b669999aa162cbb
Louisjitton <101047100+Louisjitton@users.noreply.github.com>
2022-05-11 19:02:29 +0900

    Delete DB.py

--- DB.py
+++
@@ -1,7 +0,0 @@
-#mode 작성 ; r(읽기) t(텍스트)
-#원본파일 내용을 라인별로 리스트 형태로 리턴 
-with open('input.txt', 'rt') as r:
-    book_chart = r.readlines()
-    
-#개행문자(\n) 삭제
-book_chart=[line.rstrip('\n') for line in book_chart]
